61a2d3e5a4ad42b967b01f1024a1bc0ab749a59f
Simon Bunker <simon@rendermania.com>
2021-09-24 20:08:08 +1000

    test to put update in crew module

--- crew.py
+++ crew.py
@@ -2,15 +2,60 @@ from ursina import time
 from ursina.prefabs.animation import Animation
 from ursina.prefabs.animator import Animator
 
+class moveableAnimation(Animation):
+
+    def __init__(self, animator, name, fps=12, loop=True, autoplay=True, frame_times=None, **kwargs):
+        super().__init__(name, fps=fps, loop=loop, autoplay=autoplay, frame_times=frame_times, **kwargs)
+        self.animator = animator
+
+    def step_left(self):
+        self.animator.state = "left"
+        self.resume()
+        self.x -= time.dt * 2
+
+    def step_right(self):
+        self.animator.state = "right"
+        self.resume()
+        self.x += time.dt * 2
+
+    def step_up(self):
+        self.animator.state = "up"
+        self.resume()
+        self.y += time.dt * 2
+
+    def step_down(self):
+        self.animator.state = "down"
+        self.resume()
+        self.y -= time.dt * 2
+
+    def update(self):
+
+        if held_keys["left arrow"]:
+            self.step_left()
+        elif held_keys["right arrow"]:
+            self.step_right()
+        if held_keys["up arrow"]:
+            self.step_up()
+        elif held_keys["down arrow"]:
+            self.step_down()
+        else:
+            self.pause()
+
+        # update all other animations so they stay on top of each other
+        for anim in self.animator.animations.values():
+            if anim is not self:
+                anim.x = self.x
+                anim.y = self.y
+
 class Crew(Animator):
 
     def __init__(self, name, x=0, y=0):
         super().__init__(
             animations = {
-            "left"  : Animation("assets/left", collider="box"),
-            "right" : Animation("assets/right", collider="box"),
-            "up"    : Animation("assets/up", collider="box"),
-            "down"  : Animation("assets/down", collider="box"),
+            "left"  : moveableAnimation(self, "assets/left", collider="box"),
+            "right" : moveableAnimation(self, "assets/right", collider="box"),
+            "up"    : moveableAnimation(self, "assets/up", collider="box"),
+            "down"  : moveableAnimation(self, "assets/down", collider="box"),
             })
 
         self.name = name
@@ -21,50 +66,9 @@ class Crew(Animator):
         self.mental_state = 1.0
         self.bone_density = 1.0
 
-        # positions
-        self.idle()
-        self.sprite.x = x
-        self.sprite.y = y
-        self.update_sprites()
-
     @property
-    def sprite(self):
+    def animation(self):
         return self.animations[self.state]
-
-    def update_sprites(self):
-        """Update the positions of all the Animation Sprites
-        so they stay on top of each other"""
-        for state, anim in self.animations.items():
-            if state != self.state:
-                anim.x = self.sprite.x
-                anim.y = self.sprite.y
-
-    def idle(self):
-        self.sprite.pause()
-
-    def step_left(self):
-        self.state = "left"
-        self.sprite.resume()
-        self.sprite.x -= time.dt * 2
-        self.update_sprites()
-
-    def step_right(self):
-        self.state = "right"
-        self.sprite.resume()
-        self.sprite.x += time.dt * 2
-        self.update_sprites()
-
-    def step_up(self):
-        self.state = "up"
-        self.sprite.resume()
-        self.sprite.y += time.dt * 2
-        self.update_sprites()
-
-    def step_down(self):
-        self.state = "down"
-        self.sprite.resume()
-        self.sprite.y -= time.dt * 2
-        self.update_sprites()
 
 if __name__ == "__main__":
 
@@ -75,18 +79,5 @@ if __name__ == "__main__":
     camera.fov = 10
 
     bob = Crew("bob")
-
-    def update():
-
-        if held_keys["left arrow"]:
-            bob.step_left()
-        elif held_keys["right arrow"]:
-            bob.step_right()
-        if held_keys["up arrow"]:
-            bob.step_up()
-        elif held_keys["down arrow"]:
-            bob.step_down()
-        else:
-            bob.idle()
 
     app.run()
--- spacelife.py
+++ spacelife.py
@@ -1,16 +1,15 @@
 from ursina import *
 app = Ursina()
 
+from spaceship import Spaceship
+
 camera.orthographic = True
 camera.fov = 10
 
-from spaceship import Spaceship
 ares = Spaceship()
-
-# setup spaceship
 ares.add_crew("captain", x=4, y=1)
-ares.add_crew("doctor", x=4)
-ares.add_crew("engineer", x=4, y=-1)
+# ares.add_crew("doctor", x=4)
+# ares.add_crew("engineer", x=4, y=-1)
 ares.make_active("captain")
 
 ares.add_equipment("engine")
@@ -27,36 +26,16 @@ def input(key):
     elif key == "3":
         ares.make_active("engineer")
 
-def update():
-    global ares
-
-    if held_keys["left arrow"]:
-        ares.active.step_left()
-    elif held_keys["right arrow"]:
-        ares.active.step_right()
-    if held_keys["up arrow"]:
-        ares.active.step_up()
-    elif held_keys["down arrow"]:
-        ares.active.step_down()
-    else:
-        ares.active.idle()
-
     # for obj in ares.equipment.values():
 
     #     # Update any equipment here - eg causing breakages
 
-
-    #     assigned = False
     #     for crew in ares.crew.values():
 
     #         # update crew here - eg health bars
-
-
+    
     #         if crew.intersects(obj).hit:
     #             obj.color = crew.color
     #             assigned = True
-        
-    #     if not assigned:
-    #         obj.color = color.gray
 
 app.run()
