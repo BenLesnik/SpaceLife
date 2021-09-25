8356558800387880da5b6cdee159160bec0ab779
Simon Bunker <simon@rendermania.com>
2021-09-25 22:10:07 +1000

    added labels

--- spacelife.py
+++ spacelife.py
@@ -23,23 +23,34 @@ ares.make_room("engine room")
 ares.add_equipment("engine")
 
 # Ship statistics
-oxygen = HealthBar(bar_color=color.azure, roundness=.5)
+
+Text(text="Oxygen", x=-0.85, y=0.45)
+oxygen = HealthBar(x=-0.75, y=0.45, bar_color=color.azure, roundness=.5)
 oxygen.tooltip = Tooltip('oxygen')
 oxygen.value=90
 
-fuel = HealthBar(y=0.4, bar_color=color.red, roundness=.5)
+Text(text="Fuel", x=-0.85, y=0.4)
+fuel = HealthBar(x=-0.75, y=0.4, bar_color=color.red, roundness=.5)
 fuel.tooltip = Tooltip('fuel')
 fuel.value=10
 
 # Crew statistics
-stress = HealthBar(x=0.3, bar_color=color.yellow, roundness=.5)
+Text(text="Stress", x=0.15, y=0.45)
+stress = HealthBar(x=0.35, y=0.45, bar_color=color.yellow, roundness=.5)
 stress.tooltip = Tooltip('stress')
 stress.value=50
 
-tiredness = HealthBar(x=0.3, y=0.4, bar_color=color.red, roundness=.5)
+Text(text="Tiredness", x=0.15, y=0.40)
+tiredness = HealthBar(x=0.35, y=0.4, bar_color=color.red, roundness=.5)
 tiredness.tooltip = Tooltip('tiredness')
 tiredness.value=80
 
+Text(text="Bone Density", x=0.15, y=0.35)
+bone_density = HealthBar(x=0.35, y=0.35, bar_color=color.blue, roundness=.5)
+bone_density.tooltip = Tooltip('bone density')
+bone_density.value=80
+
+# Travel duration
 timeline = HealthBar(x=-.5, y=-.4, scale_x=1, scale_y=.05, bar_color=color.lime.tint(-.25), roundness=.5, max_value=28)
 timeline.value=14
 
@@ -54,17 +65,5 @@ def input(key):
         ares.make_active("doctor")
     elif key == "3":
         ares.make_active("engineer")
-
-    # for obj in ares.equipment.values():
-
-    #     # Update any equipment here - eg causing breakages
-
-    #     for crew in ares.crew.values():
-
-    #         # update crew here - eg health bars
-    
-    #         if crew.intersects(obj).hit:
-    #             obj.color = crew.color
-    #             assigned = True
 
 app.run()
