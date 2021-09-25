941ee06c95a1d4d5660b11d0912502f2ada53afd
Simon Bunker <simon@rendermania.com>
2021-09-25 21:55:32 +1000

    added more rooms and stats

--- spacelife.py
+++ spacelife.py
@@ -17,8 +17,28 @@ ares.make_active("captain")
 
 ares.make_room("bridge")
 ares.make_room("science")
+ares.make_room("safe room")
+ares.make_room("engine room")
 
 ares.add_equipment("engine")
+
+# Ship statistics
+oxygen = HealthBar(bar_color=color.azure, roundness=.5)
+oxygen.tooltip = Tooltip('oxygen')
+oxygen.value=90
+
+fuel = HealthBar(y=0.4, bar_color=color.red, roundness=.5)
+fuel.tooltip = Tooltip('fuel')
+fuel.value=10
+
+# Crew statistics
+stress = HealthBar(x=0.3, bar_color=color.yellow, roundness=.5)
+stress.tooltip = Tooltip('stress')
+stress.value=50
+
+tiredness = HealthBar(x=0.3, y=0.4, bar_color=color.red, roundness=.5)
+tiredness.tooltip = Tooltip('tiredness')
+tiredness.value=80
 
 timeline = HealthBar(x=-.5, y=-.4, scale_x=1, scale_y=.05, bar_color=color.lime.tint(-.25), roundness=.5, max_value=28)
 timeline.value=14
