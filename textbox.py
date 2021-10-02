3ea5018f90d2eb1ff16ebd753302b57138b0b127
peterlongfield <pierre.lonchampt@gmai.com>
2021-10-02 16:18:32 +1000

    Wordwrap and size control wstill not done for text box :(

--- textbox.py
+++ textbox.py
@@ -1,5 +1,5 @@
 from ursina import *
 
 class Textbox(Entity):
-    def __init__(self, name = "name", parent=None):
-        self.name = name
+    def __init__(self, text = "Orem Lipsum", parent=None):
+        self = Text(text= text, x= 0.05, y=0.48, width = 0.2)#, height = 0.3, wordwrap = 0.7)
