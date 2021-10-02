f1efe82361adacd266d0967a1f8b27e105a65aa7
Simon Bunker <simon@rendermania.com>
2021-10-02 13:47:31 +1000

    crew can get to gym

--- crew.py
+++ crew.py
@@ -114,7 +114,7 @@ class Crew(Entity):
         s.append(Func(self.start_all_animations))
 
         # move in y from current position to centre line
-        distance_centre = pos_old_room.y
+        distance_centre = -pos_old_room.y
         distance_along = equipment.position.x - self.position.x
         distance_across = equipment.position.y
 
@@ -126,7 +126,7 @@ class Crew(Entity):
             elif distance_centre > 0:
                 s.append(Func(setattr, self.animator, "state", "up"))
 
-            s.append(Func(self.animate_y, -equipment.room.y, duration=duration, curve=curve.linear))
+            s.append(Func(self.animate_y, equipment.position.y, duration=duration, curve=curve.linear))
             s.append(duration)
 
         # move x direction along ship
