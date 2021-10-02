66454cfbab243e1c78d26e2728bf06e81805c597
peterlongfield <pierre.lonchampt@gmai.com>
2021-10-02 19:30:38 +1000

    Updated Texbox - with update function

--- textbox.py
+++ textbox.py
@@ -14,15 +14,19 @@ class Textbox(Entity):
 
 
     def __init__(self, text = "Lorem Lipsum", parent=None):
-        self.text = text
-        self = Text(text= auto_line_break(text), x= 0.05, y=0.48, width = 0.2)#, height = 0.3, wordwrap = 0.7)
+        self.text = auto_line_break(text)
+        self.disp = Text(auto_line_break(self.text), x= 0.05, y=0.48, width = 0.2, background = True)#, height = 0.3, wordwrap = 0.7)
+        
+    def disable(self):
+        print("there")
+        self.disp.disable()
+
+    def update(self, text = "new text"):
+        self.disp.disable()
+        self.text = auto_line_break(text)
+        self.disp = Text(auto_line_break(self.text), x= 0.05, y=0.48, width = 0.2, background = True)
+  
     
-    def update(newtext):
-        self.text = newtext
-        self = Text(text= auto_line_break(newtext), x= 0.05, y=0.48, width = 0.2)#, height = 0.3, wordwrap = 0.7)
-
-
 
 if __name__ == '__main__':
-    print("kjhkjhk")
     print(auto_line_break("Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",30))
